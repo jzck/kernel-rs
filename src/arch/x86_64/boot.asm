6db5a9b19d7973c916879e0f40e8a5198335615a
Jack Halford <jack@crans.org>
2018-02-04 18:51:25 +0100

    long mode achieved, starting rust

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
@@ -11,6 +12,11 @@ start:
 
 	call	set_up_page_tables
 	call	enable_paging
+
+	; load the 64-bit GDT
+	lgdt	[gdt64.pointer]
+
+	jmp		gdt64.code:long_mode_start
 
 	; print 'OK' to screen
 	mov		dword [0xb8000], 0x2f4b2f4f
@@ -138,3 +144,12 @@ p2_table:
 stack_bottom:
 	resb	64
 stack_top:
+
+section .rodata
+gdt64:
+	dq 0		; zero entry
+.code: equ $ - gdt64
+	dq (1<<43) | (1<<44) | (1<<47) | (1<<53)	; code segment
+.pointer:
+	dw $ - gdt64 - 1
+	dq gdt64
